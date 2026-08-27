e29ed75db7fdf40ede818d2a181c394bcdf3aed3
jSoteldo <georsot@gmail.com>
2026-08-27 10:45:33 -0500

    fix main.tsx

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const port = process.env.PORT || 3001;
-  await app.listen(port, '0.0.0.0');
-  console.log(`NestJS application listening on http://localhost:${port}`);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    // Aprovechamos y habilitamos CORS para cuando conectes el frontend
+    app.enableCors();
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port, '0.0.0.0');
+    console.log(`🚀 Aplicación arrancada correctamente en el puerto ${port}`);
+  } catch (error) {
+    console.error('🔥 ERROR FATAL AL ARRANCAR NESTJS:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
