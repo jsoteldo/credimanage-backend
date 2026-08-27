eb78dd6f3c048bf1637870d6f87a797f79d25402
jSoteldo <georsot@gmail.com>
2026-08-27 11:43:11 -0500

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     // Aprovechamos y habilitamos CORS para cuando conectes el frontend
-    app.enableCors();
+    app.enableCors({
+      // Agrega tu localhost (puerto 3000 para React/Next o 5173 para Vite)
+      // Asegúrate de agregar la URL pública de producción cuando despliegues tu frontend
+      origin: ['http://localhost:3000', 'http://localhost:5173', 'https://tu-dominio-frontend.vercel.app'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true, // Obligatorio si manejarás cookies o envías el JWT en las cabeceras
+    });
 
     const port = process.env.PORT || 3000;
     await app.listen(port, '0.0.0.0');
