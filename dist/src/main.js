063f736589f60746f57a3c8dcb1b9031ecd8c407
jSoteldo <georsot@gmail.com>
2026-08-27 11:18:09 -0500

    build

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,11 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
-    const port = process.env.PORT || 3001;
-    await app.listen(port, '0.0.0.0');
-    console.log(`NestJS application listening on http://localhost:${port}`);
+    try {
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        app.enableCors();
+        const port = process.env.PORT || 3000;
+        await app.listen(port, '0.0.0.0');
+        console.log(`🚀 Aplicación arrancada correctamente en el puerto ${port}`);
+    }
+    catch (error) {
+        console.error('🔥 ERROR FATAL AL ARRANCAR NESTJS:', error);
+        process.exit(1);
+    }
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
